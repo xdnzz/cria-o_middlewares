f6b287f6495f65b4c3088a8683bba5872388c620
Danilo <danilosdinizz@gmail.com>
2022-06-08 14:38:55 -0300

    finished tests

--- src/index.js
+++ src/index.js
@@ -11,17 +11,74 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const {username} = request.headers; 
+
+  const user = users.find(item => item.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: 'User not found' });
+  }
+  
+  request.user = user;
+
+  next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const {user} = request;
+
+  if(!user.pro && user.todos.length === 10) {
+    
+    return response.status(403).json({ error: 'Not available!'})
+  };
+
+  next();
+
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+  const user = users.find((user) => user.username === username);
+  
+  if (!user) {
+    return response.status(404).json({ error: "User not found" });
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "precisa ser do tipo UUID" });
+  }
+
+  const todoUser = user.todos.find((todo) => todo.id === id);
+
+  if (!todoUser) {
+    return response.status(404).json({ error: "Todo not found" });
+  }
+
+  request.todo = todoUser;
+  request.user = user;
+
+  return next();
+ 
+  
+
 }
 
 function findUserById(request, response, next) {
+const {id} = request.params;  
+
+const user = users.find(identifier => identifier.id === id);
+
+if(!user){
+  return response.status(404).json({msg:'not found'})
+};
+
+request.user = user;
+
+next();
 
 }
 
